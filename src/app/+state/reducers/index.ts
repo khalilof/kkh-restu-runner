a8d2d8aee8ce64f585d831a65c341f4146f84a5f
khalilof <khalilof@gmail.com>
2023-08-21 21:58:24 +0200

    ngrx added sync with localstorage

--- src/app/+state/reducers/index.ts
+++ src/app/+state/reducers/index.ts
@@ -1,6 +1,7 @@
-import { ActionReducerMap, MetaReducer } from '@ngrx/store';
+import { ActionReducer, ActionReducerMap, MetaReducer } from '@ngrx/store';
 import { environment } from '../../../environments/environment';
 import * as homeState from './home.reducers';
+import { storageSync } from '@larscom/ngrx-store-storagesync';
 
 export interface State {
   home: homeState.HomeState
@@ -11,4 +12,19 @@ export const reducers: ActionReducerMap<State> = {
 };
 
 
-export const metaReducers: MetaReducer<State>[] = !environment.production ? [] : [];
+export function storageSyncReducer(reducer: ActionReducer<State>): ActionReducer<State> {
+  // provide all feature states within the features array
+  // features which are not provided, do not get synced
+  const metaReducer = storageSync<State>({
+    features: [
+      // save only home state to sessionStorage
+      { stateKey: 'home', storageForFeature: window.sessionStorage },
+    ],
+    // defaults to localStorage
+    storage: window.localStorage
+  });
+
+  return metaReducer(reducer);
+}
+
+export const metaReducers: MetaReducer<State>[] = !environment.production ? [storageSyncReducer] : [storageSyncReducer];
